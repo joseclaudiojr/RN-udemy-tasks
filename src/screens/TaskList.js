2f8eb316eaaca35c806b86eb37eb2f236e1b3205
José Claudio <jose_claudiojr@hotmail.com>
2020-07-03 20:19:24 -0300

    Creation of the TaskList screen
    The TaskList is the app main screen.
    Adding Lato as the main fontFamily.

---
+++ src/screens/TaskList.js
@@ -0,0 +1,60 @@
+import React, {Component} from 'react'
+import {View, Text, ImageBackground, StyleSheet} from 'react-native'
+import commonStyles from '../commonStyles'
+import todayImage from '../../assets/imgs/today.jpg'
+
+import moment from 'moment'
+import 'moment/locale/pt-br'
+
+export default class TaskList extends Component {
+	render() {
+		const today = moment()
+			.locale('pt-br')
+			.format('ddd, D [de] MMMM')
+		return (
+			<View style={styles.container}>
+				<ImageBackground source={todayImage} style={styles.background}>
+					<View style={styles.titleBar}>
+						<Text style={styles.title}>Hoje</Text>
+						<Text style={styles.subtitle}>{today}</Text>
+					</View>
+				</ImageBackground>
+				<View style={styles.taskList}>
+					<Text>Tarefa 01</Text>
+					<Text>Tarefa 02</Text>
+					<Text>Tarefa 03</Text>
+				</View>
+			</View>
+		)
+	}
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+	},
+	background: {
+		flex: 3,
+	},
+	taskList: {
+		flex: 7,
+	},
+	titleBar: {
+		flex: 1,
+		justifyContent: 'flex-end',
+	},
+	title: {
+		fontFamily: commonStyles.fontFamily,
+		fontSize: 50,
+		color: commonStyles.colors.secondary,
+		marginLeft: 20,
+		marginBottom: 20,
+	},
+	subtitle: {
+		fontFamily: commonStyles.fontFamily,
+		color: commonStyles.colors.secondary,
+		fontSize: 20,
+		marginLeft: 20,
+		marginBottom: 20,
+	},
+})
