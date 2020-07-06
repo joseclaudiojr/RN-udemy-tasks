d716c8eb3e55a3bbecfe0373cd6747f58783a83e
José Claudio <jose_claudiojr@hotmail.com>
2020-07-06 18:19:42 -0300

    Update TaskList screen
    Showing all tasks with FlatList
    Filtering tasks by doneAt

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,84 @@
+import 'moment/locale/pt-br'
+
+import {StyleSheet, Text, TouchableWithoutFeedback, View} from 'react-native'
+
+import Icon from 'react-native-vector-icons/FontAwesome'
 import React from 'react'
-import {View, Text} from 'react-native'
+import commonStyles from '../commonStyles'
+import moment from 'moment'
+
 export default props => {
+	const doneOrNotSyle = props.doneAt != null ? styles.taskDone : {}
+	const date = props.doneAt ? props.doneAt : props.estimateAt
+	const formattedDate = moment(date)
+		.locale('pt-br')
+		.format('ddd, D [de] MMMM')
 	return (
-		<View>
-			<Text>{props.desc}</Text>
-			<Text>{props.estimateAt + ''}</Text>
-			<Text>{props + ''}</Text>
-		</View>
+		<TouchableWithoutFeedback onPress={() => props.toggleTask(props.id)}>
+			<View style={styles.container}>
+				<View style={styles.checkContainer}>{getCheckView(props.doneAt)}</View>
+				<View>
+					<Text style={[styles.desc, doneOrNotSyle]}>{props.desc}</Text>
+					<Text style={styles.date}>{formattedDate + ''}</Text>
+					{/* <Text>{props.doneAt + ''}</Text> */}
+				</View>
+			</View>
+		</TouchableWithoutFeedback>
 	)
 }
+
+function getCheckView(doneAt) {
+	if (doneAt != null) {
+		return (
+			<View style={styles.done}>
+				<Icon name="check" size={20} color={commonStyles.colors.secondary} />
+			</View>
+		)
+	} else {
+		return <View style={styles.pendding} />
+	}
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flexDirection: 'row',
+		borderColor: '#AAA',
+		borderBottomWidth: 1,
+		alignItems: 'center',
+		paddingVertical: 10,
+	},
+	checkContainer: {
+		alignItems: 'center',
+		justifyContent: 'center',
+		width: '20%',
+	},
+	pendding: {
+		height: 25,
+		width: 25,
+		borderRadius: 13,
+		borderWidth: 1,
+		borderColor: '#555',
+	},
+	done: {
+		height: 25,
+		width: 25,
+		borderRadius: 13,
+		borderWidth: 1,
+		borderColor: '#555',
+		backgroundColor: '#4D7031',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	desc: {
+		fontFamily: commonStyles.fontFamily,
+		color: commonStyles.colors.mainText,
+		fontSize: 15,
+	},
+	taskDone: {
+		textDecorationLine: 'line-through',
+	},
+	date: {
+		fontFamily: commonStyles.fontFamily,
+		fontSize: 12,
+	},
+})
--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -1,30 +1,106 @@
-import React, {Component} from 'react'
-import {View, Text, ImageBackground, StyleSheet} from 'react-native'
-import commonStyles from '../commonStyles'
-import todayImage from '../../assets/imgs/today.jpg'
-
-import moment from 'moment'
 import 'moment/locale/pt-br'
 
+import {
+	ImageBackground,
+	Platform,
+	StyleSheet,
+	Text,
+	TouchableOpacity,
+	View,
+} from 'react-native'
+import React, {Component} from 'react'
+
+import {FlatList} from 'react-native-gesture-handler'
+import Icon from 'react-native-vector-icons/FontAwesome'
+import {SafeAreaView} from 'react-navigation'
+import Task from '../components/Task'
+import commonStyles from '../commonStyles'
+import moment from 'moment'
+import todayImage from '../../assets/imgs/today.jpg'
+
 export default class TaskList extends Component {
+	state = {
+		showDoneTasks: true,
+		visibleTasks: [],
+		tasks: [
+			{
+				id: Math.random(),
+				desc: 'Comprar Livro de RN',
+				estimateAt: new Date(),
+				doneAt: new Date(),
+			},
+			{
+				id: Math.random(),
+				desc: 'Ler Livro de RN',
+				estimateAt: new Date(),
+				doneAt: null,
+			},
+		],
+	}
+
+	componentDidMount = () => {
+		this.filterTasks()
+	}
+
+	toggleFilter = () => {
+		this.setState({showDoneTasks: !this.state.showDoneTasks}, this.filterTasks)
+	}
+
+	toggleTask = taskId => {
+		const tasks = [...this.state.tasks]
+		tasks.forEach(task => {
+			if (task.id === taskId) {
+				task.doneAt = task.doneAt ? null : new Date()
+			}
+		})
+		this.setState({tasks}, this.filterTasks)
+	}
+
+	filterTasks = () => {
+		let visibleTasks = null
+		if (this.state.showDoneTasks) {
+			visibleTasks = [...this.state.tasks]
+		} else {
+			const pending = task => task.doneAt === null
+			visibleTasks = this.state.tasks.filter(pending)
+		}
+		this.setState({visibleTasks})
+	}
+
 	render() {
 		const today = moment()
 			.locale('pt-br')
 			.format('ddd, D [de] MMMM')
 		return (
-			<View style={styles.container}>
-				<ImageBackground source={todayImage} style={styles.background}>
-					<View style={styles.titleBar}>
-						<Text style={styles.title}>Hoje</Text>
-						<Text style={styles.subtitle}>{today}</Text>
+			<>
+				{/* <SafeAreaView /> */}
+				<View style={styles.container}>
+					<ImageBackground source={todayImage} style={styles.background}>
+						<View style={styles.iconBar}>
+							<TouchableOpacity onPress={this.toggleFilter}>
+								<Icon
+									name={this.state.showDoneTasks ? 'eye' : 'eye-slash'}
+									size={20}
+									color={commonStyles.colors.secondary}
+								/>
+							</TouchableOpacity>
+						</View>
+						<View style={styles.titleBar}>
+							<Text style={styles.title}>Hoje</Text>
+							<Text style={styles.subtitle}>{today}</Text>
+						</View>
+					</ImageBackground>
+					<View style={styles.taskList}>
+						<FlatList
+							data={this.state.visibleTasks}
+							keyExtractor={item => `${item.id}`}
+							renderItem={({item}) => (
+								<Task {...item} toggleTask={this.toggleTask} />
+							)}
+						/>
 					</View>
-				</ImageBackground>
-				<View style={styles.taskList}>
-					<Text>Tarefa 01</Text>
-					<Text>Tarefa 02</Text>
-					<Text>Tarefa 03</Text>
 				</View>
-			</View>
+			</>
 		)
 	}
 }
@@ -56,5 +132,11 @@ const styles = StyleSheet.create({
 		fontSize: 20,
 		marginLeft: 20,
 		marginBottom: 20,
+	},
+	iconBar: {
+		flexDirection: 'row',
+		justifyContent: 'flex-end',
+		marginHorizontal: 20,
+		marginTop: Platform.OS === 'ios' ? 40 : 10,
 	},
 })
